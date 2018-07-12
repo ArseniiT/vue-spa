2fb6a30fba30de0340b9a456e3efc645ba2be593
ArseniiT <arseniit@gmail.com>
2018-07-11 20:43:49 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+
+  extends: 'standard',
+  //required to lint *.vue files
+  plugins: [
+    'html'
+  ]
+}

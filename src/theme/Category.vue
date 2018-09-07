f78a269a81f3a53385ed086a1c58d23dac97b4fb
ArseniiT <arseniit@gmail.com>
2018-09-07 01:59:02 -0400

    Server side rendering realized

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="columns">
-    <div class="column is-one-third" v-for="(post, title) in posts" v-bind:key="post.id">
+    <div class="column is-one-third"
+    v-for="(post, title) in posts"
+    v-bind:key="post.id">
       <app-post :link="post.rest_api_enabler.Link">
         <h3 slot="title" v-html="post.title.rendered"></h3>
         <span slot="content" v-html="post.excerpt.rendered"></span>
@@ -11,26 +13,26 @@
 <script>
   import Post from './Post.vue'
   import { mapGetters } from 'vuex'
+  const fetchInitialData = (store, route) => {
+    let categoryId = 2
+    if (route.params.id === 'mobile') {
+      categoryId = 11
+    }
+    return store.dispatch('postsModule/updateCategory', categoryId)
+  }
   export default {
+    asyncData (store, route) {
+      return fetchInitialData(store, route)
+    },
     components: {
       'app-post': Post
     },
-    /* data () {
-      return {
-        id: this.$route.params.id,
-        posts: []
-      }
-    }, */
     computed: {
-      ...mapGetters('postModule', ['posts'])
+      ...mapGetters('postsModule', ['posts'])
     },
     methods: {
       loadPosts () {
-        let categoryId = 2
-        if (this.$route.params.id === 'mobile') {
-          categoryId = 11
-        }
-        this.$store.dispatch('postModule/updateCategory', categoryId)
+        fetchInitialData(this.$store, this.$route)
       }
     },
     watch: {

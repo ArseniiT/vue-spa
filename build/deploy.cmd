9dc628bfbed25d51c3118fd5d926270729625f03
ArseniiT <arseniit@gmail.com>
2018-09-13 10:13:14 -0400

    new deploy.cmd for azure

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -1,14 +1,44 @@
 call npm install
-call npm run build
+IF "%ERRORLEVEL%" NEQ "0" goto error
 
-call cd %DEPLOYMENT_TARGET%
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+call npm run build
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+for /d %%D in (%DEPLOYMENT_TARGET%\*) do (
+    if not "%%~nxD" == "node_modules" (
+        del /S /Q "%%~D"
+        IF "%ERRORLEVEL%" NEQ "0" goto error
+    )
+)
+for %%D in (%DEPLOYMENT_TARGET%\*) do (
+    del /S /Q "%%~D"
+    IF "%ERRORLEVEL%" NEQ "0" goto error
+)
 
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
 xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
+xcopy /d %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
 xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
-xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config*
 
+call cd %DEPLOYMENT_TARGET%
 call npm install --only=production
-echo Deployed.
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+goto end
+
+:error
+endlocal
+echo An error has occurred during web site deployment.
+call :exitSetErrorLevel
+call :exitFromFunction 2>nul
+
+:exitSetErrorLevel
+exit /b 1
+
+:exitFromFunction
+()
+
+:end
+endlocal
+echo Finished successfully.
